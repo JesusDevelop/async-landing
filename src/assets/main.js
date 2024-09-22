2ec87f75b188a344905c479565a16ca04cf6cf0e
Jesus Martinez <jesus258911@gmail.com>
2024-09-22 00:02:51 -0500

    fix problemas con el fetch y el API and add arreglos en el apartado de ver el video

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,4 @@
-const url = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw-ipyIc9gUWunmswPRYVAA&part=snippet%2Cid&order=date&maxResults=9';
-const content = null || document.getElementById('content');
-
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw-ipyIc9gUWunmswPRYVAA&part=snippet%2Cid&order=date&maxResults=9';
 const options = {
 	method: 'GET',
 	headers: {
@@ -8,8 +6,12 @@ const options = {
 		'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
 	}
 };
+
+
+const content = null || document.getElementById('content');
+
 async function fetchData(url) {
-    const response = await fetch(url);
+    const response = await fetch(url, options);
     const data = await response.json();
     return data;
     
@@ -17,28 +19,30 @@ async function fetchData(url) {
 
 (async () => {
     try {
-        const videos = await fetchData(url);
+        const videos = await fetchData(API);
         let view = `
         ${videos.items.map(video => `
-    <div class="group relative">
-          <div
-            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-          </div>
-          <div class="mt-4 flex justify-between">
-            <h3 class="text-sm text-gray-700">
-              <span aria-hidden="true" class="absolute inset-0"></span>
-              ${video.snippet.title}
-            </h3>
-          </div>
-        </div>
-            `).join('')}
-       
+    <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-200">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                <strong>${video.snippet.title}</strong>
+                </h3>
+            </div>
+            </div>
+        </a>
+                `).slice(0,4).join('')}
+        
         `;
         content.innerHTML =view;
         
     } catch (error){
-        
+
         console.log(error);
     }
 })();
